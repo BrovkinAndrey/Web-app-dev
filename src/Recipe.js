5a7565f8c074becce154d1ae2b7e6135cf854fed
Andrey <brovkin_58@mail.ru>
2021-04-14 09:39:39 +0300

    add css-style

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,7 +3,7 @@ import Instructions from './Instructions'
 
 const Recipe = ({ name, ingredients, steps}) =>
     <section id={name.toLowerCase().replace(/ /g, '-')}>
-        <h1>{name}</h1>
+        <h1 className="header">{name}</h1>
         <IngredientsList list={ingredients} />
         <Instructions title="Инструкция по приготовлению"
             steps={steps} />
