49c2c046127ebe92197a145571f0fe0e2b95ac48
Andrey <brovkin_58@mail.ru>
2021-04-21 22:31:28 +0300

    add input

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,22 @@
 import Recipe from './Recipe'
+import React, {useState} from 'react'
 
-const Menu = ({ recipes }) =>
+const Menu = ({ recipes }) => {
+const [recipeName, setRecipeName] = useState('');
+
+const handleInput = (e) => {
+    if (e.target.value) {
+        setRecipeName(e.target.value);
+    } else {
+        setRecipeName('');
+    }
+}
+
+return(
     <article>
         <header>
             <h1>Вкусные рецепты</h1>
+            <input type="text" value={recipeName} onChange={handleInput}/>
         </header>
         <div className="recipes">
             { recipes.map((recipe, i) =>
@@ -11,5 +24,6 @@ const Menu = ({ recipes }) =>
             }
         </div>
     </article>
-
+)
+}
 export default Menu
