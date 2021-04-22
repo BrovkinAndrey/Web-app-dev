ae40ad3c94274a63c0a99547a0b4f0930a47646d
Andrey <brovkin_58@mail.ru>
2021-04-22 10:01:41 +0300

    add useEffect

--- src/Menu.js
+++ src/Menu.js
@@ -1,14 +1,42 @@
 import Recipe from './Recipe'
-import React, {useState} from 'react'
+import RecipeList from './RecipeList'
+import React, {useState, useEffect} from 'react'
 
 const Menu = ({ recipes }) => {
-const [recipeName, setRecipeName] = useState('');
+const [inputName, setInputName] = useState('');
+const [recipeName, setRecipeName] = useState(recipes);
+
+useEffect(() => {
+    if (inputName) {
+        switch (recipeName.length) {
+            case 1: 
+                document.title = `Выбор сделан!`;
+                break;
+            case 2: 
+            case 3: 
+            case 4: 
+                document.title = `Найдено ${recipeName.length} рецепта`;
+                break;
+            default: 
+                document.title = `Найдено ${recipeName.length} рецептов`;
+        }
+    } else {
+        document.title = 'Выбери свой ужин!'
+    }
+    
+  }); 
 
 const handleInput = (e) => {
     if (e.target.value) {
-        setRecipeName(e.target.value);
+        setInputName(e.target.value);
+        const options = recipes.filter(res => {
+            const regex = new RegExp(e.target.value, 'gi');
+            return res.name.match(regex);
+        });
+        setRecipeName(options);
     } else {
-        setRecipeName('');
+        setInputName('');
+        setRecipeName(recipes);
     }
 }
 
@@ -16,14 +44,21 @@ return(
     <article>
         <header>
             <h1>Вкусные рецепты</h1>
-            <input type="text" value={recipeName} onChange={handleInput}/>
+            <input type="text" value={inputName} onChange={handleInput}/>
         </header>
-        <div className="recipes">
-            { recipes.map((recipe, i) =>
-                <Recipe key={i} {...recipe} />)
-            }
-        </div>
+        {recipeName.length === 1 
+            ?   <div className="recipes">
+                    { recipeName.map((recipe, i) =>
+                        <Recipe key={i} {...recipe} />)
+                    }
+                </div>
+            :   <div className="recipes">
+                    { recipeName.map((recipe, i) =>
+                        <RecipeList key={i} name={recipe.name} />)
+                    }
+                </div>
+        }
     </article>
-)
+    )
 }
 export default Menu
---
+++ src/RecipeList.js
@@ -0,0 +1,8 @@
+const RecipeList = ({ name }) =>
+    <ul className="RecipeList">
+        <li>
+            <span className="name">{name} </span>
+        </li>
+    </ul>
+
+export default RecipeList
